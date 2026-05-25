a1602f3fac524167ca15c247678e41c70536780e
Joffrey Demetz <joffrey.demetz@gmail.com>
2026-05-26 00:35:19 +0200

    theme via :root css vars + @container queries

--- config/jpack.js
+++ config/jpack.js
@@ -1,46 +1,17 @@
-import fs from 'fs';
-import path from 'path';
-
-import {
-    LogMe,
-    jPackConfig,
-    generateLessVariablesFromConfig,
-    deleteLessVariablesFile
-} from 'jizy-packer';
+import { jPackConfig } from 'jizy-packer';
 
 const jPackData = function () {
-    const lessBuildVariablesPath = path.join(jPackConfig.get('basePath'), 'lib/less/_variables.less');
-
     jPackConfig.sets({
         name: 'Modalizer',
         alias: 'jizy-modalizer',
-        lessVariables: {
-            desktopBreakpoint: '900px',
-            scrollbarWidth: '17px',
-            modalizerCloserColor: '#888',
-            modalizerLayer: 10000,
-            modalizerWidth: '700px',
-            modalizerWidthMax: '1200px'
-        },
         defaults: {
         }
     });
 
     jPackConfig.set('onCheckConfig', () => { });
-
-    jPackConfig.set('onGenerateBuildJs', (code) => {
-        LogMe.log('Build lib/less/_variables.less');
-        const lessVariables = jPackConfig.get('lessVariables') ?? {};
-        const lessOriginalVariablesPath = path.join(jPackConfig.get('basePath'), 'lib/less/variables.less');
-        generateLessVariablesFromConfig(lessOriginalVariablesPath, lessBuildVariablesPath, lessVariables);
-        return code;
-    });
-
+    jPackConfig.set('onGenerateBuildJs', (code) => code);
     jPackConfig.set('onGenerateWrappedJs', (wrapped) => wrapped);
-
-    jPackConfig.set('onPacked', () => {
-        deleteLessVariablesFile(lessBuildVariablesPath);
-    });
+    jPackConfig.set('onPacked', () => { });
 };
 
 export default jPackData;
